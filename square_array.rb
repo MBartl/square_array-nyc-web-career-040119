7fb7c3b2317fc272900ba02365391e1f0862a69b
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 19:22:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
-  new_array = []
   array.each do |number|
-    new_array.push(array[number] * array[number])
+    array[number] * array[number]
   end
-  return new_array
 end
